b24254d5efd8db4dfed5fbfc4a8e437a97eafc03
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-04-05 14:21:48 -0300

    Adicionando tratamento de erro no Event

--- src/models/repository/events_repository.py
+++ src/models/repository/events_repository.py
@@ -1,5 +1,8 @@
 from typing import Dict
 
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
+
 from src.models.entities.events import Events
 from src.models.settings.connection import db_connection_handler
 
@@ -7,18 +10,26 @@ from src.models.settings.connection import db_connection_handler
 class EventsRepository:
     def insert_event(self, eventsInfo:Dict) -> Dict:
         with db_connection_handler as database:
-            event = Events(
-                id=eventsInfo.get('uuid'),
-                title=eventsInfo.get('title'),
-                details=eventsInfo.get('details'),
-                slug=eventsInfo.get('slug'),
-                maximum_attendees=eventsInfo.get('maximum_attendees')
-            )
-            database.session.add(event)
-            database.session.commit()
-            return eventsInfo
-    
+            try:
+                event = Events(
+                    id=eventsInfo.get('uuid'),
+                    title=eventsInfo.get('title'),
+                    details=eventsInfo.get('details'),
+                    slug=eventsInfo.get('slug'),
+                    maximum_attendees=eventsInfo.get('maximum_attendees')
+                )
+                database.session.add(event)
+                database.session.commit()
+                return eventsInfo
+            except IntegrityError:
+                raise Exception('Evento já cadastrado!')
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
     def get_event_by_id(self, event_id: str) -> Dict:
         with db_connection_handler as database:
-            event = database.session.query(Events).filter(Events.id == event_id).one()
-            return event
+            try:
+                event = database.session.query(Events).filter(Events.id == event_id).one()
+                return event
+            except NoResultFound:
+                return None
