71cb0b9bfe62a39ba22e9f35471ad18dad471dba
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-04-05 18:11:52 -0300

    conecção com apí

--- src/main/routes/event_routes.py
+++ src/main/routes/event_routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
+from src.data.event_handler import EventHandler
 from src.http_types.http_request import HttpRequest
 
 event_route_bp = Blueprint('event_route', __name__)
@@ -7,4 +8,7 @@ event_route_bp = Blueprint('event_route', __name__)
 @event_route_bp.route('/events', methods=['POST'])
 def create_event():
     http_request = HttpRequest(body=request.json)
-    return jsonify({'ola': 'mundo'}), 200
+    event_handler = EventHandler()
+    
+    http_response = event_handler.register(http_request)
+    return jsonify(http_response.body), http_response.status_code
